cf3ad864d1a65f27de285a5b3a319608de049479
fistki <38302858+fistki@users.noreply.github.com>
2021-04-15 18:05:45 +0900

    d

--- NameCard/NameCard/NameCard.cpp
+++ NameCard/NameCard/NameCard.cpp
@@ -4,7 +4,8 @@
 int main(void)
 {
 	ArrayList<NameCard*> list;
-	NameCard* _outNameCard;
+	NameCard* _outpNameCard;
+
 	//1. 총 3명의 전화번호 정보를, 리스트에 저장
 
 	list.LInsert(makeNameCard("Mike", "01012345678"));
@@ -14,39 +15,45 @@ int main(void)
 
 	//2. 특정 이름을 대상으로 탐색을 진행하여, 그 사람의 정보를 출력
 	
-	if (list.LFirst(_outNameCard))
+	if (list.LFirst(_outpNameCard))
 	{
-		showCardInfo(*_outNameCard);
+		if(_outpNameCard->getName() == "Ho")
+			showCardInfo(*_outpNameCard);
 
-		while(list.LNext(_outNameCard))
-			showCardInfo(*_outNameCard);
+		while(list.LNext(_outpNameCard))
+			if (_outpNameCard->getName() == "Ho") {
+				showCardInfo(*_outpNameCard);
+				break;
+			}
 	}
 
 	//3. 특정 이름을 대상으로 탐색을 진행하여, 그 사람의 전화번호 변경
 
-	if (list.LFirst(_outNameCard))
+	if (list.LFirst(_outpNameCard))
 	{
-		if (_outNameCard->getName() == "John")
-			_outNameCard->changePhoneNum("01099998888");
-		while (list.LNext(_outNameCard))
+		if (_outpNameCard->getName() == "Mike")
+			_outpNameCard->changePhoneNum("01099998888");
+		while (list.LNext(_outpNameCard))
 		{
-			if (_outNameCard->getName() == "John")
-				_outNameCard->changePhoneNum("01099998888");
+			if (_outpNameCard->getName() == "Mike")
+				_outpNameCard->changePhoneNum("01099998888");
+			break;
 		}
 	}
 
 
 	//4. 특정 이름을 대상으로 탐색을 진행하여, 그 사람의 정보 삭제
 
-	if (list.LFirst(_outNameCard))
+	if (list.LFirst(_outpNameCard))
 	{
-		if (_outNameCard->getName() == "Ho") {
+		if (_outpNameCard->getName() == "John") {
 			delete list.LRemove();
 		}
-		while (list.LNext(_outNameCard))
+		while (list.LNext(_outpNameCard))
 		{
-			if (_outNameCard->getName() == "Ho") {
+			if (_outpNameCard->getName() == "John") {
 				delete list.LRemove();
+				break;
 			}
 		}
 	}
@@ -54,12 +61,12 @@ int main(void)
 
 	//5. 남아 있는 모든 사람의 전화번호를 출력
 
-	if (list.LFirst(_outNameCard))
+	if (list.LFirst(_outpNameCard))
 	{
-		showCardInfo(*_outNameCard);
-		while (list.LNext(_outNameCard))
+		showCardInfo(*_outpNameCard);
+		while (list.LNext(_outpNameCard))
 		{
-			showCardInfo(*_outNameCard);
+			showCardInfo(*_outpNameCard);
 		}
 	}
 
